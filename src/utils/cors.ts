1bc1736c77bc3524bffe98941251aa99686fee5d
CommerceBankUSA <pavelflorin326@gmail.com>
2025-10-27 10:05:13 +0100

    fix: edit the cors origin

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -2,7 +2,7 @@ import { FastifyCorsOptions } from "@fastify/cors";
 
 export const allowedOrigins = [
   "http://localhost:5173",
-  "https://sage-heliotrope-306e7e.netlify.app",
+  "https://cbshvault.com",
 ];
 
 export const corsOptions: FastifyCorsOptions = {
